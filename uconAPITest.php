78bb1ee1a6fc1245833a3ccfd19f069a13562f3c
JaBray <jabraymc@gmail.com>
2020-02-17 18:36:02 -0500

    Update uconAPITest.php
    Added a space before "Table"

--- uconAPITest.php
+++ uconAPITest.php
@@ -63,7 +63,7 @@
 		if (mysqli_num_rows($result) > 0) {
 			// output data of each row
 			while($row = mysqli_fetch_assoc($result)) {
-				echo "Name: " . $row["s_title"]. " <br> Time: " . $row["i_time"]. "<br> Room " . $row["id_room"]. "Table ". $row["s_table"];
+				echo "Name: " . $row["s_title"]. " <br> Time: " . $row["i_time"]. "<br> Room " . $row["id_room"]. " Table ". $row["s_table"];
 			}
 		} else {
 			echo "0 results";
