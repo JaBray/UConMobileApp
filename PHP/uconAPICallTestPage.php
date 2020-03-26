f5a926f2ba28095164770f05bce4c8d57d1b9f5f
JaBray <jabraymc@gmail.com>
2020-03-25 21:26:21 -0400

    Update uconAPICallTestPage.php

--- PHP/uconAPICallTestPage.php
+++ PHP/uconAPICallTestPage.php
@@ -7,8 +7,8 @@
 
 <body>
 
-<form action="uconAPITest.php" method="post">
-User ID: <input type="text" name="userID">
+<form action="uconAPITest.php" method="get">
+User ID: <input type="text" name="user_id">
 <input type="submit" value="Submit">
 
 </form>
