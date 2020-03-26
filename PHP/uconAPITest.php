6e4ed0e7544484c90abb477f21049707902aecf1
JaBray <jabraymc@gmail.com>
2020-03-25 21:25:43 -0400

    Overhauls API

--- PHP/uconAPITest.php
+++ PHP/uconAPITest.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
 <?php
+	
+	//Sets the response type to json
+	header('Content-Type: application/json');
+
+//Database credentials
+//TODO replace with adoDB creds
 	$servername = "localhost";
 	$username = "admin";
 	$password = "password";
 	$dbname = "ucon";
 
-//Create connection to database using provided inputs
+	//Create connection to database using provided inputs
 	$conn = new mysqli($servername, $username,$password,$dbname);
+	//Grabs the current year
 	//TODO Remove the minus one
 	$year = date("Y") - 1;
 
@@ -16,24 +20,15 @@
 	if($conn->connect_error){
 		die("Connenction to database failed. Error: ".$conn->connect_error);
 	}
-	
-	
-	
-	
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	//If the user ID is supplied and is not empty
+	if(isset($_GET['user_id']) && $_GET['user_id']!=""){
+		$userId = $_GET['user_id'];
+		getUserEvents($userId);
 			
-			getUserEvents($_POST['userID']);
-		
 	}
 
 
-
-
-
-	
-	
-	
-	
 	// A method that finds all events being attended and hosted by user
 	function getUserEvents($userId){ // takes  the User ID 
 		global $conn;
@@ -45,20 +40,23 @@
 		$result = $stmt->get_result();
 			
 			
-		// This calls the getEventInfo for all events being attended
+		//Create user object to store data
+		$userObj = new stdClass();
+		$userObj->userId = $userId;
+		//Create schedule arrays
+		$attendeeSchedule=array();
+		$gamemasterSchedule=array();
+			
+			
+		// This calls the getEventInfo for all events being attended and adds them to the array
 		if (mysqli_num_rows($result) > 0) {
-			echo "<b>List of $userId's $year events that they are attending: <br><br></b>";
+			
 			while($row = mysqli_fetch_assoc($result)) {
 				$event = $row["s_subtype"];
-				getEventInfo((int)$event);
+				array_push($attendeeSchedule, getEventInfo((int)$event));
 			}
-		
 		}
 		
-		
-
-		else{
-		echo "No events listed for $userId in $year";}
 		// Finding all of the games being ran by the userId
 		$stmt = $conn->prepare("SELECT id_event FROM ucon_event WHERE id_gm = ? AND id_convention = ? ORDER BY e_day, i_time ASC");
 		$stmt->bind_param("ss", $userId, $year);
@@ -67,20 +65,29 @@
 		
 		
 		
-		// Same loop that instead runs the event id's that the User is running
+		// Same loop that instead runs the event id's that the user is running and adds them to the array
 		if (mysqli_num_rows($result) > 0) {
-			echo "<b> List of $userId's $year events that they are Game Master of: <br><br></b>";
+	
 			while($row = mysqli_fetch_assoc($result)) {
 				$event = $row["id_event"];
-				getEventInfo((int)$event);
+				array_push($gamemasterSchedule, getEventInfo((int)$event));
 			}
 		
 		}
 		
+		//Creates and echoes the json object
+		$userObj->eventYear = $year;
+		$userObj->attendeeSchedule = $attendeeSchedule;
+		$userObj->gamemasterSchedule = $gamemasterSchedule;
+		
+		echo json_encode($userObj);
+		
+		
+		
 	}
 		
 		
-	//Return event information given the event id
+	//Return event information object given the event id
 	function getEventInfo($eventId){
 		global $conn;
 		
@@ -89,37 +96,27 @@
 		$stmt->execute();
 		$result = $stmt->get_result();
 		
-		// I added a day to be displayed for the event.
+		
 	
 		$eventObj = new stdClass();
 		if (mysqli_num_rows($result) > 0) {
 			
 			while($row = mysqli_fetch_assoc($result)) {
+				$eventObj->eventId = $eventId;
 				$eventObj->title = $row["s_title"];
 				$eventObj->day = $row["e_day"];
 				$eventObj->time = $row["i_time"];
 				$eventObj->room = $row["id_room"];
 				$eventObj->table = $row["s_table"];
-				
-				$eventJSON = json_encode($eventObj);
-				echo $eventJSON."<br>";
+
 			}
-		} else {
-			echo "0 results";
-			}
-		
-		
+			
+			
+		} 
+		return $eventObj;
 	}
 
+	
+
+
 ?>
-
-</head>
-
-
-<body>
-	<button onclick="goBack()">Go Back</button>
-</body>
-
-
-
-</html>
