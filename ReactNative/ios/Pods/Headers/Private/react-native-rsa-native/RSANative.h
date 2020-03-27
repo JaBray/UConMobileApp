223cfeb34da886f77eb82452831c1f5f0ef02ae1
EbrahimQasem <eaaqasem@gmail.com>
2020-03-27 11:37:31 -0400

    Added ios configs
    - Changed the version of ios
    - 'pod install' inside ios folder

---
+++ ReactNative/ios/Pods/Headers/Private/react-native-rsa-native/RSANative.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-rsa-native/ios/RSANative.h
---
+++ ReactNative/ios/Pods/Target Support Files/react-native-rsa-native/react-native-rsa-native-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_react_native_rsa_native : NSObject
+@end
+@implementation PodsDummy_react_native_rsa_native
+@end
