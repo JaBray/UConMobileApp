516d23c78dd93710d96707e26494eda1abeb4d9a
pandaimonia <56488013+pandaimonia@users.noreply.github.com>
2020-04-01 17:32:20 -0400

    Splits events into Friday/Saturday/Sunday

--- ReactNative/components/Schedule.js
+++ ReactNative/components/Schedule.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import { View, ScrollView, FlatList, SafeAreaView, StyleSheet, Text } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import Header from './Header';
@@ -12,8 +12,12 @@ export default class Schedule extends Component {
     super(props);
 
     const events = this._getSchedule();
+    const friday = this._getFriday();
+    const saturday = this._getSaturday();
+    const sunday = this._getSunday();
     // THE events STATE IS THE ARRAY OF EVENTS THAT IS PASSED TO THE FLATLIST
     this.state = { events: events };
+    this.setState({ scrollEnabled: false });
   }
 
   // THE FLATLIST IS THE REAL ENGINE OF THIS VIEW. IT DISPLAYS A LIST OF
@@ -26,25 +30,73 @@ export default class Schedule extends Component {
       <View style={styles.container}>
         <SafeAreaView style={styles.container}>
           <Header />
-          <FlatList
-            data={this.state.events}
-            // add event properties inside <Event>
-            renderItem={({item}) =>
-            <Event
-              title={item.title}
-              day={item.day}
-              time={item.time}
-              e_time={item.e_time}
-              s_fname={item.s_fname}
-              s_lname={item.s_lname}
-              i_maxplayers={item.i_maxplayers}
-              e_exper={item.e_exper}
-              e_complex={item.e_complex}
-              s_room={item.s_room}
-            />}
-            keyExtractor={(item, index) => index.toString()}
-          />
-          <MyLargeButton title="Logout" press={this.props.onLogout}/>
+          <ScrollView style={styles.viewStyle}>
+
+            <Text style={styles.dayStyle}> FRIDAY</Text>
+            <FlatList
+              scrollEnabled={this.state.scrollEnabled}
+              data={this.state.friday}
+              // add event properties inside <Event>
+              renderItem={({item}) =>
+              <Event
+                title={item.title}
+                day={item.day}
+                time={item.time}
+                e_time={item.e_time}
+                s_fname={item.s_fname}
+                s_lname={item.s_lname}
+                i_maxplayers={item.i_maxplayers}
+                e_exper={item.e_exper}
+                e_complex={item.e_complex}
+                s_room={item.s_room}
+              />}
+              keyExtractor={(item, index) => index.toString()}
+            />
+
+            <Text style={styles.dayStyle}> SATURDAY</Text>
+            <FlatList
+              scrollEnabled={this.state.scrollEnabled}
+              data={this.state.saturday}
+              // add event properties inside <Event>
+              renderItem={({item}) =>
+              <Event
+                title={item.title}
+                day={item.day}
+                time={item.time}
+                e_time={item.e_time}
+                s_fname={item.s_fname}
+                s_lname={item.s_lname}
+                i_maxplayers={item.i_maxplayers}
+                e_exper={item.e_exper}
+                e_complex={item.e_complex}
+                s_room={item.s_room}
+              />}
+              keyExtractor={(item, index) => index.toString()}
+            />
+
+            <Text style={styles.dayStyle}> SUNDAY</Text>
+            <FlatList
+              scrollEnabled={this.state.scrollEnabled}
+              data={this.state.sunday}
+              // add event properties inside <Event>
+              renderItem={({item}) =>
+              <Event
+                title={item.title}
+                day={item.day}
+                time={item.time}
+                e_time={item.e_time}
+                s_fname={item.s_fname}
+                s_lname={item.s_lname}
+                i_maxplayers={item.i_maxplayers}
+                e_exper={item.e_exper}
+                e_complex={item.e_complex}
+                s_room={item.s_room}
+              />}
+              keyExtractor={(item, index) => index.toString()}
+            />
+
+            <MyLargeButton title="Logout" style={styles.button} press={this.props.onLogout}/>
+          </ScrollView>
         </SafeAreaView>
       </View>
     );
@@ -56,6 +108,39 @@ export default class Schedule extends Component {
     const events = JSON.parse(await AsyncStorage.getItem(id));
     this.setState({events: events});
   }
+  //modified _getSchedule, deletes saturday and sunday events leaving only friday
+  _getFriday = async () => {
+    const id = this.props.memberId;
+    const friday = JSON.parse(await AsyncStorage.getItem(id));
+    for (var i = friday.length - 1; i >= 0; --i) {
+      if (friday[i].day == "Saturday" || friday[i].day == "Sunday") {
+          friday.splice(i,1);
+      }
+    }
+    this.setState({friday: friday});
+  }
+  //saturday events
+  _getSaturday = async () => {
+    const id = this.props.memberId;
+    const saturday = JSON.parse(await AsyncStorage.getItem(id));
+    for (var i = saturday.length - 1; i >= 0; --i) {
+      if (saturday[i].day == "Friday" || saturday[i].day == "Sunday") {
+          saturday.splice(i,1);
+      }
+    }
+    this.setState({saturday: saturday});
+  }
+  //sunday events
+  _getSunday = async () => {
+    const id = this.props.memberId;
+    const sunday = JSON.parse(await AsyncStorage.getItem(id));
+    for (var i = sunday.length - 1; i >= 0; --i) {
+      if (sunday[i].day == "Saturday" || sunday[i].day == "Friday") {
+          sunday.splice(i,1);
+      }
+    }
+    this.setState({sunday: sunday});
+  }
 }
 
 const styles = StyleSheet.create({
@@ -65,5 +150,18 @@ const styles = StyleSheet.create({
   navigation: {
     flexDirection: 'row',
     justifyContent: 'space-around'
+  },
+  dayStyle: {
+    marginHorizontal: 5,
+    marginVertical:5,
+    backgroundColor: '#3F3F3F',
+    color: 'white',
+    height: 40,
+    fontSize:25,
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+  button: {
+    marginVertical:5
   }
 });
