eb62e105a03b95656c8f9e06e469c1d4e9fe288e
JaBray <jabraymc@gmail.com>
2020-04-16 21:24:57 -0400

    Update uconAPITest.php
    Updates all uses of MYSQLnd to use ADOdb connections instead.

--- PHP/uconAPITest.php
+++ PHP/uconAPITest.php
@@ -3,41 +3,42 @@
 	//Sets the response type to json
 	header('Content-Type: application/json');
 
+	//Includes adodb library https://adodb.org/dokuwiki/doku.php
+  require_once 'adodb5/adodb.inc.php';
+
 //Database credentials
-//TODO replace with adoDB creds
 	$servername = "localhost";
 	$username = "admin";
 	$password = "password";
 	$dbname = "ucon";
+//Sets the connection type for the database and connects
+  $driver = 'mysqli';
+  $db = newAdoConnection($driver);
+  $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+  $db->connect('localhost','admin','password','ucon');
 
-	//Create connection to database using provided inputs
-	$conn = new mysqli($servername, $username,$password,$dbname);
 	//Grabs the current year
 	//TODO Remove the minus one
 	$year = date("Y") - 1;
 
-	//Kill connection in case of error and print error message
-	if($conn->connect_error){
-		die("Connenction to database failed. Error: ".$conn->connect_error);
-	}
-
-	//If the user ID is supplied and is not empty
+	//If the user ID is supplied and is not empty, store the ID and displays the resulting schedule JSON from getUserEvents
 	if(isset($_GET['user_id']) && $_GET['user_id']!=""){
 		$userId = $_GET['user_id'];
-		getUserEvents($userId);
+		echo getUserEvents($userId);
 
 	}
 
 
-	// A method that finds all events being attended and hosted by user
+	// Method takes one parameter, UserID, and returns a JSON object of the user and their schedules
 	function getUserEvents($userId){ // takes  the User ID
-		global $conn;
+		//Database connection object and year
+		global $db;
 		global $year;
-		// finding all of the tickets purchased for games by user id
-		$stmt = $conn->prepare("SELECT s_subtype FROM ucon_order WHERE id_member = ? AND s_type = 'Ticket' AND id_convention = ?");
-		$stmt->bind_param("ss", $userId, $year);
-		$stmt->execute();
-		$result = $stmt->get_result();
+
+		//Grabs order information from database
+    $sql = "SELECT s_subtype FROM ucon_order WHERE id_member = ? AND s_type = 'Ticket' AND id_convention = ?";
+    $bindVars = array($userId, $year);
+		$result = $db->execute($sql, $bindVars);
 
 
 		//Create user object to store data
@@ -49,63 +50,58 @@
 
 
 		// This calls the getEventInfo for all events being attended and adds them to the array
-		if (mysqli_num_rows($result) > 0) {
-
-			while($row = mysqli_fetch_assoc($result)) {
+		if ($result->recordCount() > 0) {
+			//Iterate through the result and add the event info for each event into the schedule
+			while($row = $result->fetchRow()) {
 				$event = $row["s_subtype"];
 				array_push($attendeeSchedule, getEventInfo((int)$event));
 			}
 		}
 
-		// Finding all of the games being ran by the userId
-		$stmt = $conn->prepare("SELECT id_event FROM ucon_event WHERE id_gm = ? AND id_convention = ? ORDER BY e_day, i_time ASC");
-		$stmt->bind_param("ss", $userId, $year);
-		$stmt->execute();
-		$result = $stmt->get_result();
+		// Finds all events for which the supplied user ID is GameMaster
+    $sql = "SELECT id_event FROM ucon_event WHERE id_gm = ? AND id_convention = ? ORDER BY e_day, i_time ASC";
+		$result = $db->execute($sql, $bindVars);
 
 
 
-		// Same loop that instead runs the event id's that the user is running and adds them to the array
-		if (mysqli_num_rows($result) > 0) {
+		// Iterate through result and grab event information
+		if ($result->recordCount() > 0) {
 
-			while($row = mysqli_fetch_assoc($result)) {
+			while($row = $result->fetchRow()) {
 				$event = $row["id_event"];
 				array_push($gamemasterSchedule, getEventInfo((int)$event));
 			}
 
 		}
 
-		//Creates and echoes the json object
+		//Bundles and returns the json object
 		$userObj->eventYear = $year;
 		$userObj->attendeeSchedule = $attendeeSchedule;
 		$userObj->gamemasterSchedule = $gamemasterSchedule;
 
-		echo json_encode($userObj);
+		return json_encode($userObj);
 
 
 
 	}
 
 
-	//Return event information object given the event id
+	//Takes one parameter, eventID, and returns an object with event information
 	function getEventInfo($eventId){
-		global $conn;
+		global $db;
+		//Select relevant fields from database given the event ID
+    $sql = "SELECT s_title, s_game, i_time, e_day, id_room, s_table, s_desc, i_maxplayers, e_exper, e_complex, i_length, id_gm FROM ucon_event WHERE id_event = ?";
+    $result = $db->execute($sql, $eventId);
 
-		$stmt = $conn->prepare("SELECT s_title, s_game, i_time, e_day, id_room, s_table, s_desc, i_maxplayers, e_exper, e_complex, i_length, id_gm FROM ucon_event WHERE id_event = ? ORDER BY e_day, i_time ASC");
-		$stmt->bind_param("s",$eventId);
-		$stmt->execute();
-		$result = $stmt->get_result();
-
-
-
+		//Create a basic object to store the data
 		$eventObj = new stdClass();
-		if (mysqli_num_rows($result) > 0) {
+
 
 			//Bind each of the values from the result into a single object
-			$row = mysqli_fetch_assoc($result);
+			$row = $result->fetchRow();
 				$eventObj->eventId = $eventId;
 				$eventObj->title = $row["s_title"];
-				//s_game
+
 				$eventObj->game = $row["s_game"];
 
 				$eventObj->day = $row["e_day"];
@@ -122,25 +118,25 @@
 
 				// make sure its not table
 
-		}
+
 		return $eventObj;
 	}
 
-//Returns the name of the given user
-	function getGameMasterName($id_gm){
-		global $conn;
-		$stmt = $conn->prepare("SELECT s_fname, s_lname FROM ucon_member WHERE id_member = ?");
-		$stmt->bind_param("i",$id_gm);
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$row = mysqli_fetch_assoc($result);
+//Takes on parameter, GM ID, and returns the first and last name of the user with the given ID
+function getGameMasterName($id_gm){
+  global $db;
+  $sql = "SELECT s_fname, s_lname FROM ucon_member WHERE id_member = ?";
+  $result = $db->execute($sql, $id_gm);
+  $row = $result->fetchRow();
 
-		if (mysqli_num_rows($result) > 0) {
-			$name = $row["s_fname"].$row["s_lname"];
-	}
-		return $name;
 
-	}
+
+
+    $name = $row["s_fname"]." ".$row["s_lname"];
+
+  return $name;
+
+}
 
 
 
